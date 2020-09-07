99fb66eb0d4d981838505ffa9349fc774a813397
Zagura <me@zagura.one>
2020-09-07 15:02:34 +0200

    added more sources

--- app/src/main/java/posidon/launcher/feed/news/chooser/suggestions/Suggestions.kt
+++ app/src/main/java/posidon/launcher/feed/news/chooser/suggestions/Suggestions.kt
@@ -6,10 +6,15 @@ import posidon.launcher.tools.Tools
 object Suggestions {
 
     val list = mapOf(
-        "Tech (English)" to R.raw.suggestions_tech_en,
         "United States" to R.raw.suggestions_united_states,
+        "Spain" to R.raw.suggestions_spain,
+        "Russia" to R.raw.suggestions_russia,
+        "India" to R.raw.suggestions_india,
+        "Ukraine" to R.raw.suggestions_ukraine,
+        "Tech (English)" to R.raw.suggestions_tech_en,
         "Tech (Spanish)" to R.raw.suggestions_tech_es,
-        "Spain" to R.raw.suggestions_spain
+        "Tech (Italian)" to R.raw.suggestions_tech_it,
+        "Tech (Gujarati)" to R.raw.suggestions_tech_gu
     )
 
     val topics by lazy { list.map {
