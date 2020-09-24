5442ce8b7ffa5d7cb00de24a5d68317bd9ca55c5
leoxshn <leo@posidon.io>
2020-09-24 08:44:21 +0200

    fixed some bugs

--- app/src/main/java/posidon/launcher/customizations/CustomGestures.kt
+++ app/src/main/java/posidon/launcher/customizations/CustomGestures.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import posidon.launcher.Global
 import posidon.launcher.R
 import posidon.launcher.tools.Tools
 import posidon.launcher.tools.applyFontSetting
@@ -16,5 +17,11 @@ class CustomGestures : AppCompatActivity() {
         setContentView(R.layout.custom_gestures)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         findViewById<View>(R.id.settings).setPadding(0, 0, 0, Tools.navbarHeight)
+    }
+
+    override fun onPause() {
+        Global.shouldSetApps = true
+        Global.customized = true
+        super.onPause()
     }
 }
